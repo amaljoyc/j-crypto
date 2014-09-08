db09e1a118c07846876d866c07134f5a7f1e55dd
amaljoyc <amaljoyc@gmail.com>
2014-09-08 22:48:04 +0530

    key computation first version

--- src/test/Key.java
+++ src/test/Key.java
@@ -2,7 +2,12 @@ package test;
 
 public class Key {
 	public static void main(String[] args) {
-		String key = args[0];
+		String key = "Abc";
 		System.out.println(key);
+		StringBuilder sb = new StringBuilder();
+		for(int a: key.toCharArray()) {
+			sb.append(a);
+		}
+		key = sb.reverse().toString();
 	}
 }
